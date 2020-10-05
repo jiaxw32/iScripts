c68be96944ca43297bf8ec28a4fc50393e0ffdec
jiaxuewen <jiaxuewen@58.com>
2020-10-05 17:37:25 +0800

    get path info

--- frida/appinfo.js
+++ frida/appinfo.js
@@ -2,6 +2,78 @@ Module.ensureInitialized("UIKit");
 Module.ensureInitialized("CoreGraphics");
 Module.ensureInitialized("AdSupport");
 
+function exportFunction(type, name, ret, args) {
+  var nptr;
+  nptr = Module.findExportByName(null, name);
+  if (nptr === null) {
+      console.log("cannot find " + name);
+      return null;
+  } else {
+      if (type === "f") {
+          var funclet = new NativeFunction(nptr, ret, args);
+          if (typeof funclet === "undefined") {
+              console.log("parse error " + name);
+              return null;
+          }
+          return funclet;
+      } else if (type === "d") {
+          var datalet = Memory.readPointer(nptr);
+          if (typeof datalet === "undefined") {
+              console.log("parse error " + name);
+              return null;
+          }
+          return datalet;
+      }
+  }
+}
+
+var NSSearchPathForDirectoriesInDomains = exportFunction("f", "NSSearchPathForDirectoriesInDomains", "pointer", ["int", "int", "int"]);
+var NSHomeDirectory = exportFunction("f", "NSHomeDirectory", "pointer", []);
+var NSTemporaryDirectory = exportFunction("f", "NSTemporaryDirectory", "pointer", []);
+var NSUserName = exportFunction("f", "NSUserName", "pointer", []);
+var NSFullUserName = exportFunction("f", "NSFullUserName", "pointer", []);
+
+function userName() {
+  var dir = NSUserName();
+  return ObjC.Object(dir).toString();
+}
+
+function fullUserName() {
+  var dir = NSFullUserName();
+  return ObjC.Object(dir).toString();
+}
+
+function documentDirectory() {
+  var NSDocumentDirectory = 9;
+  var NSUserDomainMask = 1;
+  var npdirs = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, 1);
+  return ObjC.Object(npdirs).objectAtIndex_(0).toString();
+}
+
+function libraryDirectory() {
+  var NSLibraryDirectory = 5;
+  var NSUserDomainMask = 1;
+  var npdirs = NSSearchPathForDirectoriesInDomains(NSLibraryDirectory, NSUserDomainMask, 1);
+  return ObjC.Object(npdirs).objectAtIndex_(0).toString();
+}
+
+function cachesDirectory() {
+  var NSCachesDirectory = 13;
+  var NSUserDomainMask = 1;
+  var npdirs = NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, 1);
+  return ObjC.Object(npdirs).objectAtIndex_(0).toString();
+}
+
+function homeDirectory() {
+  var dir = NSHomeDirectory();
+  return ObjC.Object(dir).toString();
+}
+
+function temporaryDirectory() {
+  var dir = NSTemporaryDirectory();
+  return ObjC.Object(dir).toString();
+}
+
 var UIDevice = ObjC.classes.UIDevice;
 var currentDevice = UIDevice.currentDevice();
 
@@ -20,103 +92,183 @@ function bundleInfoForKey(key) {
   return mainBundle.infoDictionary().objectForKey_(key).toString();
 }
 
+function deviceName(){
+  return currentDevice.name().toString();
+}
+
+function systemName() {
+  return currentDevice.systemName().toString();
+}
+
+function systemVersion() {
+  return currentDevice.systemVersion().toString();
+}
+
+function deviceModel() {
+  return currentDevice.model().toString();
+}
+
+function localizedModel() {
+  return currentDevice.localizedModel().toString();
+}
+
+function identifierForVendor() {
+  return currentDevice.identifierForVendor().UUIDString().toString();
+}
+
+function advertisingIdentifier(){
+  return ASIdentifierManager.sharedManager().advertisingIdentifier().UUIDString().toString();
+}
+
+function batteryLevel() {
+  // Battery level ranges from 0.0 (fully discharged) to 1.0 (100% charged). Before accessing this property, ensure that battery monitoring is enabled.
+  // If battery monitoring is not enabled, the value of this property is –1.0.
+  return currentDevice.batteryLevel();
+}
+
+function batteryState() {
+  var state = currentDevice.batteryState();
+  var ret = "unknown";
+  switch (state) {
+    case 0:
+      ret = "unknown";
+      break;
+    case 1:
+      ret = "unplugged";
+      break;
+    case 2:
+      ret = "charging";
+      break;
+    case 3:
+      ret = "full";
+      break;
+    default:
+      break;
+  }
+  return ret;
+}
+
+function screenWidth() {
+  var bounds = mainScreen.bounds();
+  var size = bounds[1];
+  return size[0];
+}
+
+function screenHeight() {
+  var bounds = mainScreen.bounds(); //bounds is a CGRect struct
+  var size = bounds[1];
+  return size[1];
+}
+
+function screenWidthInPixels() {
+  var size = mainScreen.currentMode().size();
+  return size[0];
+}
+
+function screenHeightInPixels() {
+  var size = mainScreen.currentMode().size();
+  return size[1];
+}
+
+function screenScale() {
+  return mainScreen.scale();
+}
+
+function hostName() {
+  return processInfo.hostName().toString();
+}
+
+function processName() {
+  return processInfo.processName().toString();
+}
+
+function processIdentifier() {
+  return processInfo.processIdentifier();
+}
+
+function osVersionString() {
+  return processInfo.operatingSystemVersionString().toString();
+}
+
+function bundleIdentifier() {
+  return mainBundle.bundleIdentifier().toString();
+}
+
+function bundlePath() {
+  return mainBundle.bundlePath().toString();
+}
+
+function sharedFrameworksPath() {
+  return mainBundle.sharedFrameworksPath().toString();
+}
+
+function privateFrameworksPath() {
+  return mainBundle.privateFrameworksPath().toString();
+}
+
+function bundleName() {
+  return bundleInfoForKey("CFBundleName").toString();
+}
+
+function bundleDisplayName() {
+  return bundleInfoForKey("CFBundleDisplayName");
+}
+
+function executableFile() {
+  return bundleInfoForKey("CFBundleExecutable");
+}
+
+function executablePath(){
+  return NSBundle.mainBundle().executablePath().toString();
+}
+
+function receiptPath() {
+  return mainBundle.appStoreReceiptURL().path().toString();
+}
+
+function appShortVersion() {
+  return bundleInfoForKey("CFBundleShortVersionString");
+}
+
+function appVersion() {
+  return bundleInfoForKey("CFBundleVersion");
+}
+
 rpc.exports = {
-  devicename: function () {
-    return currentDevice.name().toString();
-  },
-  systemname: function () {
-    return currentDevice.systemName().toString();
-  },
-  systemversion: function () {
-    return currentDevice.systemVersion().toString();
-  },
-  model: function () {
-    return currentDevice.model().toString();
-  },
-  localizedmodel: function () {
-    return currentDevice.localizedModel().toString();
-  },
-  idfv: function () {
-    return currentDevice.identifierForVendor().UUIDString().toString();
-  },
-  idfa: function(){
-    return ASIdentifierManager.sharedManager().advertisingIdentifier().UUIDString().toString();
-  },
-  batterylevel: function () {
-    // Battery level ranges from 0.0 (fully discharged) to 1.0 (100% charged). Before accessing this property, ensure that battery monitoring is enabled.
-    // If battery monitoring is not enabled, the value of this property is –1.0.
-    return currentDevice.batteryLevel();
-  },
-  batterystate: function () {
-    var state = currentDevice.batteryState();
-    var ret = "unknown";
-    switch (state) {
-      case 0:
-        ret = "unknown";
-        break;
-      case 1:
-        ret = "unplugged";
-        break;
-      case 2:
-        ret = "charging";
-        break;
-      case 3:
-        ret = "full";
-        break;
-      default:
-        break;
-    }
-    return ret;
-  },
-  screenwidth: function () {
-    var bounds = mainScreen.bounds();
-    var size = bounds[1];
-    return size[0];
-  },
-  screenheight: function () {
-    var bounds = mainScreen.bounds(); //bounds is a CGRect struct
-    var size = bounds[1];
-    return size[1];
-  },
-  screenwidthinpixels: function () {
-    var size = mainScreen.currentMode().size();
-    return size[0];
-  },
-  screenheightinpixels: function () {
-    var size = mainScreen.currentMode().size();
-    return size[1];
-  },
-  scale: function () {
-    return mainScreen.scale();
-  },
-  hostname: function () {
-    return processInfo.hostName().toString();
-  },
-  processname: function () {
-    return processInfo.processName().toString();
-  },
-  processid: function () {
-    return processInfo.processIdentifier();
-  },
-  osversion: function () {
-    return processInfo.operatingSystemVersionString().toString();
-  },
-  bundleid: function () {
-    return mainBundle.bundleIdentifier().toString();
-  },
-  bundlename: function () {
-    return bundleInfo.objectForKey_("CFBundleName").toString();
-  },
-  appname: function () {
-    return bundleInfoForKey("CFBundleDisplayName");
-  },
-  executablefile: function () {
-    return bundleInfoForKey("CFBundleExecutable");
-  },
-  appshortversion: function () {
-    return bundleInfoForKey("CFBundleShortVersionString");
-  },
-  appversion: function () {
-    return bundleInfoForKey("CFBundleVersion");
-  },
+  devicename: deviceName,
+  systemname: systemName,
+  systemversion: systemVersion,
+  model: deviceModel,
+  localizedmodel: localizedModel,
+  idfv: identifierForVendor,
+  idfa: advertisingIdentifier,
+  batterylevel: batteryLevel, 
+  batterystate: batteryState,
+  screenwidth: screenWidth,
+  screenheight: screenHeight,
+  screenwidthinpixels: screenWidthInPixels,
+  screenheightinpixels: screenHeightInPixels,
+  scale: screenScale,
+  hostname: hostName,
+  processname: processName,
+  processid: processIdentifier,
+  osversion: osVersionString,
+  bundleid: bundleIdentifier,
+  bundlename: bundleName,
+  appname: bundleDisplayName,
+  executablefile: executableFile,
+  executablepath: executablePath,
+  appshortversion: appShortVersion,
+  appversion: appVersion,
+  sharedframeworkspath: sharedFrameworksPath,
+  privateframeworkspath: privateFrameworksPath,
+  bundlepath: bundlePath,
+  receiptpath: receiptPath,
+  homedir: homeDirectory,
+  docdir: documentDirectory,
+  tempidr: temporaryDirectory,
+  cachesdir: cachesDirectory,
+  librarydir: libraryDirectory,
+  username: userName,
+  fullusername: fullUserName
 };
--- frida/appinfo.py
+++ frida/appinfo.py
@@ -1,6 +1,7 @@
 import codecs
 import frida
 import json
+import threading
 
 def on_message(message, data):
     if message['type'] == 'send':
@@ -8,7 +9,35 @@ def on_message(message, data):
     elif message['type'] == 'error':
         print(message['stack'])
 
-session = frida.get_usb_device().attach(u'抖音短视频')
+def get_usb_iphone():
+    Type = 'usb'
+    if int(frida.__version__.split('.')[0]) < 12:
+        Type = 'tether'
+    device_manager = frida.get_device_manager()
+    changed = threading.Event()
+
+    def on_changed():
+        changed.set()
+
+    device_manager.on('changed', on_changed)
+
+    device = None
+    while device is None:
+        devices = [dev for dev in device_manager.enumerate_devices() if dev.type == Type]
+        if len(devices) == 0:
+            print('Waiting for USB device...')
+            changed.wait()
+        else:
+            device = devices[0]
+
+    device_manager.off('changed', on_changed)
+
+    return device
+
+device = get_usb_iphone()   
+pid = device.get_frontmost_application().pid
+print(f"proccess id: {pid}")
+session = device.attach(pid)
 with codecs.open('./appinfo.js', 'r', 'utf-8') as f:
     source = f.read()
 script = session.create_script(source)
@@ -16,6 +45,8 @@ script.on('message', on_message)
 script.load()
 
 deviceinfo: dict = {}
+deviceinfo["username"] = script.exports.username();
+deviceinfo["fullusername"] = script.exports.fullusername();
 deviceinfo["device_name"] = script.exports.devicename();
 deviceinfo["system_name"] = script.exports.systemname();
 deviceinfo["system_version"] = script.exports.systemversion();
@@ -41,7 +72,17 @@ appInfo: dict = {}
 appInfo["bundleid"] = script.exports.bundleid();
 appInfo["appname"] = script.exports.appname();
 appInfo["bundlename"] = script.exports.bundlename();
+appInfo["bundle_path"] = script.exports.bundlepath();
 appInfo["executable_file"] = script.exports.executablefile();
+appInfo["executable_path"] = script.exports.executablepath();
+appInfo["receipt_path"] = script.exports.receiptpath();
+appInfo["share_frameworks_path"] = script.exports.sharedframeworkspath();
+appInfo["private_frameworks_path"] = script.exports.privateframeworkspath();
+appInfo["home_directory"] = script.exports.homedir();
+appInfo["document_directory"] = script.exports.docdir();
+appInfo["temporary_directory"] = script.exports.tempidr();
+appInfo["caches_directory"] = script.exports.cachesdir();
+appInfo["library_directory"] = script.exports.librarydir();
 appInfo["app_version"] = script.exports.appversion();
 appInfo["app_short_version"] = script.exports.appshortversion();
 appInfo["idfv"] = script.exports.idfv();

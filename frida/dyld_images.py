cef80ed7c1e04d7ac6c35546b1de0c1fa904fc42
jiaxw32 <jiaxw32@gmail.com>
2020-10-10 12:06:39 +0800

    optimize get modules info.

--- frida/dyld_images.py
+++ frida/dyld_images.py
@@ -1,11 +1,19 @@
+import sys
 import frida
 
 def on_message(message, data):
     print("[on_message] message:", message, "data:", data)
 
 # session = frida.attach(u"微信") # Mac 应用程序
+# session = frida.get_usb_device().attach(u'抖音短视频')
 
-session = frida.get_usb_device().attach(u'抖音短视频')
+device = frida.get_usb_device()
+application = device.get_frontmost_application()
+if application is None:
+  print("please run the application first.")
+  sys.exit(-1)
+pid = application.pid
+session = device.attach(pid)
 
 script = session.create_script("""
 rpc.exports.enumerateModules = function () {
@@ -20,8 +28,6 @@ for module in script.exports.enumerate_modules():
     path = module["path"]
     base_addr = module["base"]
     size = module["size"]
-    
     # print(f"type of module: {type(module)}")
-    
     if ".app" in path:
       print(f"image name: {name}\nbase address: {base_addr}\nimage size: {size}\nimage path: {path}\n")

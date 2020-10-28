242690f5d2c2ca2eeee4ffc4517e456caf89f829
jiaxw32 <jiaxw32@gmail.com>
2020-10-28 20:12:45 +0800

    support AFNetworking old version API.

--- frida/AFNetWorking/AFHTTPSessionManager.js
+++ frida/AFNetWorking/AFHTTPSessionManager.js
@@ -11,17 +11,10 @@ if (ObjC.available) {
     var NSError = ObjC.classes.NSError;
     var errorPtr = Memory.alloc(Process.pointerSize);
     Memory.writePointer(errorPtr, NULL); // initialize to NULL
-    var data = NSJSONSerialization.dataWithJSONObject_options_error_(
-      obj,
-      NSJSONWritingPrettyPrinted,
-      errorPtr
-    );
+    var data = NSJSONSerialization.dataWithJSONObject_options_error_(obj, NSJSONWritingPrettyPrinted, errorPtr);
     var error = Memory.readPointer(errorPtr);
     if (error.isNull()) {
-      var str = NSString.alloc().initWithData_encoding_(
-        data,
-        NSUTF8StringEncoding
-      );
+      var str = NSString.alloc().initWithData_encoding_(data, NSUTF8StringEncoding);
       return str.toString();
     } else {
       var errorObj = new ObjC.Object(error); // now you can treat errorObj as an NSError instance
@@ -43,30 +36,38 @@ if (ObjC.available) {
     var pendingBlocks = [];
     var AFHTTPSessionManager = ObjC.classes.AFHTTPSessionManager;
     printMethodsOfClass(AFHTTPSessionManager);
-    var dataTaskWithHTTPMethod_ =
-      AFHTTPSessionManager[
-        "- dataTaskWithHTTPMethod:URLString:parameters:headers:uploadProgress:downloadProgress:success:failure:"
-      ];
+    var isNewAPI = true;
+    //new version api
+    var dataTaskWithHTTPMethod_ = AFHTTPSessionManager["- dataTaskWithHTTPMethod:URLString:parameters:headers:uploadProgress:downloadProgress:success:failure:"];
     if (typeof dataTaskWithHTTPMethod_ == 'undefined') {
-      console.log('-[AFHTTPSessionManager dataTaskWithHTTPMethod:URLString:parameters:headers:uploadProgress:downloadProgress:success:failure:] undefined.');
-    } else {
-      Interceptor.attach(dataTaskWithHTTPMethod_.implementation, {
-        onEnter: function (args, state) {
-          // var method = ObjC.Object(args[2]);
-          // var url = ObjC.Object(args[3]);
-          // var params = ObjC.Object(args[4]);
-          // if (NSJSONSerialization.isValidJSONObject_(params)) {
-          //   params = convertNSObjectToJSString(params);
-          // }
-          // var headers = ObjC.Object(args[5]);
-          // if (NSJSONSerialization.isValidJSONObject_(headers)) {
-          //   headers = convertNSObjectToJSString(headers);
-          // }
-  
-          var successBlock = new ObjC.Block(args[8]);
-          pendingBlocks.push(successBlock);
-          var successBlockIMP = successBlock.implementation;
-          successBlock.implementation = function (task, response) {
+      //old version api
+      dataTaskWithHTTPMethod_ = AFHTTPSessionManager["- dataTaskWithHTTPMethod:URLString:parameters:uploadProgress:downloadProgress:success:failure:"];
+      isNewAPI = false;
+    }
+    Interceptor.attach(dataTaskWithHTTPMethod_.implementation, {
+      onEnter: function (args, state) {
+        var method = ObjC.Object(args[2]);
+        var url = ObjC.Object(args[3]);
+        var params = ObjC.Object(args[4]);
+        if (NSJSONSerialization.isValidJSONObject_(params)) {
+          params = convertNSObjectToJSString(params);
+        }
+        // var headers = ObjC.Object(args[5]);
+        // if (NSJSONSerialization.isValidJSONObject_(headers)) {
+        //   headers = convertNSObjectToJSString(headers);
+        // }
+        var successBlock = isNewAPI ? (new ObjC.Block(args[8])) : (new ObjC.Block(args[7]));
+        
+        pendingBlocks.push(successBlock);
+        var successBlockIMP = successBlock.implementation;
+        successBlock.implementation = function (task, response) {
+          var method_signature = '-[AFHTTPSessionManager dataTaskWithHTTPMethod:URLString:parameters:' + (isNewAPI ? 'header:' : '') + 'uploadProgress:downloadProgress:success:failure:]'
+          if (response == null) {
+            console.log(method_signature + ' success callback\n' + 
+              'url: ' + url.toString() + '\n' +
+              'method: ' + method.toString() + '\n' + 
+              'response: null' + '\n');
+          } else {
             var responseObject = ObjC.Object(response);
             var nstask = ObjC.Object(task);
             var request = nstask.currentRequest();
@@ -79,67 +80,35 @@ if (ObjC.available) {
               responseObject = convertNSObjectToJSString(responseObject);
             }
             console.log(
-              "-[AFHTTPSessionManager dataTaskWithHTTPMethod:URLString:parameters:headers:uploadProgress:downloadProgress:success:failure:] success callback\n" +
+              method_signature + ' success callback\n' +
                 "url: " + jsurl + "\n" +
                 "method: " + jsmethod + "\n" +
                 "headers: " + convertNSObjectToJSString(headerFields) + "\n" +
                 "body: " + body + '\n' +
-                "response: " + responseObject
+                "response: " + responseObject + '\n'
             );
-            successBlockIMP(task, response);
-            const idx = pendingBlocks.indexOf(successBlock);
-            delete pendingBlocks[idx];
-          };
-          // console.log(
-          //   `-[AFHTTPSessionManager dataTaskWithHTTPMethod:URLString:parameters:headers:uploadProgress:downloadProgress:success:failure:]\nurl: ${url}\nmethod: ${method}\nparams: ${params}\nheaders: ${headers}`
-          // );
-        },
-      });
-    }
+          }
+          successBlockIMP(task, response);
+          const idx = pendingBlocks.indexOf(successBlock);
+          delete pendingBlocks[idx];
+        };
+      },
+    });
 
-    var postWithConstructBody =
-      AFHTTPSessionManager[
-        "- POST:parameters:headers:constructingBodyWithBlock:progress:success:failure:"
-      ];
+    var postWithConstructBody = AFHTTPSessionManager["- POST:parameters:headers:constructingBodyWithBlock:progress:success:failure:"];
     if (typeof postWithConstructBody == 'undefined') {
-      console.log('-[AFHTTPSessionManager POST:parameters:headers:constructingBodyWithBlock:progress:success:failure:] undefined.');
-    } else {
-      Interceptor.attach(postWithConstructBody.implementation, {
-        onEnter: function (args, state) {
-          var url = ObjC.Object(args[2]);
-          var params = ObjC.Object(args[3]);
-          if (NSJSONSerialization.isValidJSONObject_(params)) {
-            params = convertNSObjectToJSString(params);
-          }
-          var headers = ObjC.Object(args[4]);
-          if (NSJSONSerialization.isValidJSONObject_(headers)) {
-            headers = convertNSObjectToJSString(headers);
-          }
-  
-          var successBlock = new ObjC.Block(args[7]);
-          var successBlockIMP = successBlock.implementation;
-          successBlock.implementation = function (task, response) {
-            var responseObject = ObjC.Object(response);
-            var nstask = ObjC.Object(task);
-            var request = nstask.currentRequest();
-            var jsurl = request.URL().absoluteString().toString();
-            if (NSJSONSerialization.isValidJSONObject_(responseObject)) {
-              responseObject = convertNSObjectToJSString(responseObject);
-            }
-            console.log(
-              "-[AFHTTPSessionManager POST:parameters:headers:constructingBodyWithBlock:progress:success:failure:] success callback\nurl:" +
-                jsurl +
-                "\n" +
-                responseObject
-            );
-            successBlockIMP(task, response);
-          };
-          console.log(
-            `-[AFHTTPSessionManager POST:parameters:headers:constructingBodyWithBlock:progress:success:failure:]\nurl: ${url}\nparams: ${params}\nheaders: ${headers}`
-          );
-        },
-      });
+      postWithConstructBody = AFHTTPSessionManager["- POST:parameters:constructingBodyWithBlock:progress:success:failure:"];
     }
+    Interceptor.attach(postWithConstructBody.implementation, {
+      onEnter: function (args, state) {
+        var url = ObjC.Object(args[2]);
+        var params = ObjC.Object(args[3]);
+        if (NSJSONSerialization.isValidJSONObject_(params)) {
+          params = convertNSObjectToJSString(params);
+        }
+        console.log(`-[AFHTTPSessionManager POST:parameters:constructingBodyWithBlock:progress:success:failure:]\nurl: ${url}\nparams: ${params}\nheaders: ${headers}`);
+      },
+    });
   } else {
     console.log("the current app doesn't include AFHTTPSessionManager class.");
   }

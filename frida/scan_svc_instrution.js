2af662412ca8246a2001bc0832bfc4079fd738e9
jiaxw32 <jiaxw32@gmail.com>
2022-11-08 18:15:31 +0800

    memory scan svc instruction

--- frida/scan_svc_instrution.js
+++ frida/scan_svc_instrution.js
@@ -1,24 +1,30 @@
-const mainModule = Process.enumerateModulesSync()[0];
-var startAddr = mainModule.base;
-var endAddr = startAddr.add(mainModule.size);
-console.log("Module name: " + mainModule.name + ", size: " + mainModule.size);
-console.log('base address: ' + startAddr + ', end address: ' + endAddr);
-
-// console.log(hexdump(mainModule.base));
+var NSBundle = ObjC.classes.NSBundle;
+var path = NSBundle.mainBundle().bundlePath().toString();
+console.log("bundle path: " + path);
 
 var addr = Module.findExportByName("libSystem.B.dylib", "_dyld_get_image_vmaddr_slide");
 var _dyld_get_image_vmaddr_slide = new NativeFunction(addr, "long", ["uint"]);
-var slide = _dyld_get_image_vmaddr_slide(0);
-console.log("slide: 0x" + slide.toString(16));
 
 const pattern = '01 10 00 D4'; // svc 0x80
 
-Memory.scan(mainModule.base, mainModule.size, pattern, {
-    onMatch(address, size) {
-       const offset = address.sub(slide);
-       console.log('found svc instruction at', address, ", offset: ", offset);
-    },
-    onComplete() {
-      console.log('Memory.scan() complete');
-    }
+Process.enumerateModulesSync().forEach((module, index) => {
+  var slide = _dyld_get_image_vmaddr_slide(index);
+  // var startAddr = module.base;
+  // var endAddr = startAddr.add(module.size);
+  // console.log("Module name: " + module.name + ", size: " + module.size);
+  // console.log('base address: ' + startAddr + ', end address: ' + endAddr + ", slide: 0x" + slide.toString(16));
+
+  if (module.path.indexOf(path) != -1) {
+    console.log(module.path);
+    Memory.scan(module.base, module.size, pattern, {
+      onMatch(address, size) {
+        const offset = address.sub(slide);
+        console.log('module: ' + module.name + ', found svc instruction at ' + address + ", offset: " + offset);
+        console.log(hexdump(address.sub(0x20), {length: 48, ansi: false, header: false }), "\n");
+      },
+      onComplete() {
+        console.log('Memory.scan() complete');
+      }
+    });
+  }
 });
